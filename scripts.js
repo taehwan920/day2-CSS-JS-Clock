8f71ec3b393adb9f6aa826e1890c49e1fa68fcc8
Yang Taehwan <taehwan920@gmail.com>
2020-03-15 16:18:45 +0900

    for now, no freezing when webpage is loaded and no weird hand-moves when hands point top

--- scripts.js
+++ scripts.js
@@ -4,17 +4,35 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if (secondsDegrees === 90) {
+        secondHand.style.transition = 'all 0.001s';
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+    } else {
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    }
+
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    if (minutesDegrees === 90) {
+        minuteHand.style.transition = 'all 0.001s';
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+    } else {
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    }
 
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    if (hoursDegrees === 90) {
+        hourHand.style.transition = 'all 0.001s';
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+    } else {
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    }
 }
 
+setDate();
 setInterval(setDate, 1000);
